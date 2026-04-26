4cf82b0f50fbe66da1313c566e80e10930d7ee85
Olena Chornobai <s32774@pjwstk.edu.pl>
2026-04-26 12:28:19 +0200

    Add appointment details endpoint

--- Controllers/AppointmentsController.cs
+++ Controllers/AppointmentsController.cs
@@ -65,4 +65,67 @@ public class AppointmentsController : ControllerBase
 
         return Ok(appointments);
     }
+
+    [HttpGet("{idAppointment:int}")]
+    public async Task<IActionResult> GetAppointmentById(int idAppointment)
+    {
+        await using var connection = new SqlConnection(_connectionString);
+        await connection.OpenAsync();
+
+        await using var command = new SqlCommand("""
+            SELECT
+                a.IdAppointment,
+                a.AppointmentDate,
+                a.Status,
+                a.Reason,
+                a.InternalNotes,
+                a.CreatedAt,
+                p.IdPatient,
+                p.FirstName + N' ' + p.LastName AS PatientFullName,
+                p.Email AS PatientEmail,
+                p.PhoneNumber AS PatientPhoneNumber,
+                d.IdDoctor,
+                d.FirstName + N' ' + d.LastName AS DoctorFullName,
+                d.LicenseNumber AS DoctorLicenseNumber,
+                s.Name AS SpecializationName
+            FROM dbo.Appointments a
+            JOIN dbo.Patients p ON p.IdPatient = a.IdPatient
+            JOIN dbo.Doctors d ON d.IdDoctor = a.IdDoctor
+            JOIN dbo.Specializations s ON s.IdSpecialization = d.IdSpecialization
+            WHERE a.IdAppointment = @IdAppointment;
+            """, connection);
+
+        command.Parameters.Add("@IdAppointment", SqlDbType.Int).Value = idAppointment;
+
+        await using var reader = await command.ExecuteReaderAsync();
+
+        if (!await reader.ReadAsync())
+        {
+            return NotFound(new { message = "Appointment not found." });
+        }
+
+        var appointment = new AppointmentDetailsDto
+        {
+            IdAppointment = reader.GetInt32(reader.GetOrdinal("IdAppointment")),
+            AppointmentDate = reader.GetDateTime(reader.GetOrdinal("AppointmentDate")),
+            Status = reader.GetString(reader.GetOrdinal("Status")),
+            Reason = reader.GetString(reader.GetOrdinal("Reason")),
+            InternalNotes = reader.IsDBNull(reader.GetOrdinal("InternalNotes"))
+                ? null
+                : reader.GetString(reader.GetOrdinal("InternalNotes")),
+            CreatedAt = reader.GetDateTime(reader.GetOrdinal("CreatedAt")),
+
+            IdPatient = reader.GetInt32(reader.GetOrdinal("IdPatient")),
+            PatientFullName = reader.GetString(reader.GetOrdinal("PatientFullName")),
+            PatientEmail = reader.GetString(reader.GetOrdinal("PatientEmail")),
+            PatientPhoneNumber = reader.GetString(reader.GetOrdinal("PatientPhoneNumber")),
+
+            IdDoctor = reader.GetInt32(reader.GetOrdinal("IdDoctor")),
+            DoctorFullName = reader.GetString(reader.GetOrdinal("DoctorFullName")),
+            DoctorLicenseNumber = reader.GetString(reader.GetOrdinal("DoctorLicenseNumber")),
+            SpecializationName = reader.GetString(reader.GetOrdinal("SpecializationName"))
+        };
+
+        return Ok(appointment);
+    }
 }
